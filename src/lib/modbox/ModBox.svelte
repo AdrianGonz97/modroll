9efd886214acd1cce44c532bc77612f082ade079
cokakoala <adriangonz457@gmail.com>
2021-10-13 15:29:47 -0400

    added basic fram of modbox

---
+++ src/lib/modbox/ModBox.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { spring } from 'svelte/motion';
+	export let num;
+</script>
+
+<div class="name-container">
+	<label>
+		<span>{num}</span>
+		<input name="text" aria-labelledby="Add todo" placeholder="Empty" />
+	</label>
+</div>
+
+<style>
+	input {
+	}
+	label {
+	}
+	span {
+	}
+</style>
---
+++ src/lib/modbox/ModContainer.svelte
@@ -0,0 +1,9 @@
+<script>
+	import ModBox from './ModBox.svelte';
+
+	export let count;
+</script>
+
+<!-- {#each } -->
+<style>
+</style>
