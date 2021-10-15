ba213233f58e9b0b69506979a4f64dea2dd29480
cokakoala <adriangonz457@gmail.com>
2021-10-15 14:27:45 -0400

    setup for gh pages

---
+++ gh-pages.js
@@ -0,0 +1,17 @@
+import { publish } from 'gh-pages';
+
+publish(
+	'build', // path to public directory
+	{
+		branch: 'gh-pages',
+		repo: 'https://github.com/AdrianGonz97/modroll-web-app',
+		user: {
+			name: 'Adrian',
+			email: 'cokakoala.dev@gmail.com'
+		},
+		dotfiles: true
+	},
+	() => {
+		console.log('Deploy Complete!');
+	}
+);
