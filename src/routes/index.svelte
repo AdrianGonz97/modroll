8317349044e5fd7e15c4fd9642bd143127950297
cokakoala <adriangonz457@gmail.com>
2021-10-27 10:58:22 -0400

    removed unneeded code

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -14,15 +14,7 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<!-- <div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div> -->
-		Rolly for Le Mod
-	</h1>
+	<h1>Rolly for Le Mod</h1>
 
 	<h2>Edit the numbers below to adjust the quantity</h2>
 	<div class="counters">
@@ -37,7 +29,6 @@
 	section {
 		display: flex;
 		flex-direction: column;
-		/* justify-content: center; */
 		align-items: center;
 		flex: 1;
 	}
@@ -52,18 +43,4 @@
 		justify-content: center;
 		align-items: center;
 	}
-	/* .welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	} */
 </style>
